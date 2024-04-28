8cae90c21e28f98e6182c5aadb83775215814763
Cem Bıçakcı <cmbicakci@gmail.com>
2024-04-28 13:42:48 +0300

    initial commit

---
+++ instaClone/instaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instaCloneApp.swift
+//  instaClone
+//
+//  Created by Cem Bıçakcı on 28.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct instaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
